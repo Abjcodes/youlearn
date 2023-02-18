30e37ea4f22be870a66fffc3adfcb24274e01646
unknown <abijithmv01@gmail.com>
2023-02-18 23:37:29 +0530

    Fixed notes

--- components/TextEditor.jsx
+++ components/TextEditor.jsx
@@ -6,57 +6,84 @@ import Placeholder from '@tiptap/extension-placeholder'
 import { supabase } from '../supabaseClient'
 import { useUser } from '@supabase/auth-helpers-react'
 import { useEffect, useState } from 'react'
+import { createEditor } from "@tiptap/core";
 
 const TextEditor= ({id, title}) => {
-    const user = useUser();
-    const [loading, setLoading] = useState(false);
-    const [content, setContent] = useState([]);
-    const [notes, setNotes] = useState([]);
 
-    const loadContent = async () => {
-     await fetchNotes();
-      if(notes != null){
-        const result = notes.find(note => {
-          return note.video_id === id;
-        })
-        setContent(result);
+  const [noteContent, setNoteContent] = useState("");
+  const [notesExist, setNotesExist] = useState(false);
+
+  useEffect(() => {
+    async function fetchNoteContent() {
+      const { data, error } = await supabase
+        .from("notes")
+        .select("notes")
+        .eq("video_id", id)
+        .single();
+
+      if (error) {
+        console.log("error", error);
+      } else {
+        setNoteContent(data.notes);
       }
     }
 
-    useEffect(() => {
-      loadContent();
-    }, [])
-
-    const fetchNotes = async () => {
-      try {
-          const{data, error} = await supabase
-              .from("notes")
-              .select("*")
-          if(error) throw error;
-          if(data != null){
-              setNotes(data);
-              console.log(data);
-          }
-      } catch (error) {
-          alert(error.message);
-      }
-    };
+    fetchNoteContent();
+  }, [id, noteContent]);
 
     const editor = useEditor({
-        extensions: [
-          StarterKit,
-          Highlight,
-          Typography,
-          Placeholder.configure({
-            placeholder: 'Write your notes here …',
-          })
-        ],
-        editorProps: {
-            attributes: {
-              class: 'prose m-3 focus:outline-none',
-            },
+      extensions: [
+        StarterKit,
+        Highlight,
+        Typography,
+        Placeholder.configure({
+          placeholder: 
+          'If you have taken notes for this video before, you can load them with the load button. The button wont be visible otherwise.\nMarkdown Controls\n- Text Formatting\ncntrl+I for italics, cntrl+B for bold\n- Headings\n#, ##, ###, ####, #####, ###### for different levels\n- Other formatting\n> for blockquotes, *, - or + for bullet lists, or `foobar` to highlight code, ~~tildes~~ to strike text, or ==equal signs== to highlight text.\n'
+          // - `Code` with `backticks`
+          
+          // ## Headings
+          // - # H1 with `# space`
+          // - ## H2 with `## space`
+          // - ### H3 with `### space`
+          
+          // ## Lists
+          // - Unordered list with `-` or `*` and a space
+          //   - Nested items with 2 spaces before the dash
+          // - Ordered list with numbers and a period
+          // 1. First item
+          // 2. Second item',
+        })
+      ],
+      editorProps: {
+          attributes: {
+            class: 'prose m-3 focus:outline-none',
           },
-      });
+        },
+    });
+
+    
+    
+    const user = useUser();
+    const [loading, setLoading] = useState(false);
+    const [notes, setNotes] = useState([]);
+    
+
+    // const fetchNotes = async () => {
+    //   try {
+    //       const{data, error} = await supabase
+    //           .from("notes")
+    //           .select("*")
+    //       if(error) throw error;
+    //       if(data != null){
+    //           setNotes(data);
+    //           console.log(data);
+    //       }
+    //   } catch (error) {
+    //       alert(error.message);
+    //   }
+    // };
+
+    
 
     
     
@@ -79,6 +106,23 @@ const TextEditor= ({id, title}) => {
           setLoading(false);
         }
       };
+
+      const loadNotes = () => {
+        editor.commands.setContent(noteContent);
+        setNotesExist(true);
+      }
+
+    //   useEffect(() => {
+    //   if(noteContent != null){
+    //   if(editor){
+    //     editor.on('create', ({ editor }) => {
+    //       editor.commands.insertContent(noteContent);
+    //       editor.commands.setContent('<p>Example Text</p>')
+    //       alert("editor created");
+    //     })
+    //   }
+    // }
+    // }, [editor, noteContent])
 
 
     
@@ -109,11 +153,20 @@ const TextEditor= ({id, title}) => {
             </h1>
             <EditorContent editor={editor}/>
           </div>
+          <div className='flex w-full'>
           <button type="button" 
-          className="text-white bg-black hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium text-sm px-5 py-2.5 text-center mx-2 mb-2"
+          className=" w-full text-white bg-black hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium text-sm px-5 py-2.5 text-center mx-2 mb-2"
           onClick={() => saveNotes()}>
             {loading ? "Saving..." : "Save notes"}
           </button>
+          {noteContent!="" ? 
+          <button type='button' className='w-1/4 text-black border-2 border-black hover:bg-gradient-to-br font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2'  
+          onClick={() => loadNotes()}>
+            Load
+          </button>
+          : null
+          }
+          </div>
             </div> 
             :
             <div> 
--- pages/notes.js
+++ pages/notes.js
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../supabaseClient";
 import { useUser } from "@supabase/auth-helpers-react";
+import Link from "next/link";
 
 const NotesPage = () => {
   const [notes, setNotes] = useState([]);
   const user = useUser();
-
+  const [showModal, setShowModal] = useState(false);
+  let emailId = null;
   useEffect(() => {
-    fetchNotes();
-  }, []);
+    if(user) {
+      emailId = user.email;
+    }
+    fetchNotes(emailId);
+  }, [user]);
 
-  const fetchNotes = async () => {
+  const fetchNotes = async (email) => {
     try {
         const{data, error} = await supabase
             .from("notes")
             .select("*")
         if(error) throw error;
-        if(data != null){
-            setNotes(data);
+        if(data != null && emailId!= null){
+          const filteredArr = data.filter((note) => {
+            return note.user_id === email;
+          })
+            setNotes(filteredArr);
+            
         }
     } catch (error) {
         alert(error.message);
@@ -28,14 +37,22 @@ const NotesPage = () => {
     <div className="flex flex-wrap">
       {user ? 
       notes.map(note => (
-        <div key={note.user_id + note.video_id} className="w-full lg:w-1/3 p-4">
-          <div className="bg-white shadow-lg rounded-lg overflow-hidden h-48">
-            <div className="p-6">
-              <h2 className="text-base font-medium">{note.video_title}</h2>
-              <h3 className="text-sm py-1 text-slate-600">{note.created_at}</h3>
-              <div className="content text-base text-gray-600 overflow-hidden" dangerouslySetInnerHTML={{__html: note.notes}}></div>
+        <div key={note.user_id + note.video_id} className="w-full lg:w-1/4 p-4">
+          <Link key={note.videoId}
+          href={{
+            pathname:'/viewer',
+            query: {videoId: note.video_id, videoTitle: note.video_title}
+          }}>
+          <div className="bg-white shadow-lg rounded-lg overflow-auto h-56" onClick={() => setShowModal(true)}>
+            <div className="p-6 h-full">
+              <h2 className="text-base font-bold">{note.video_title}</h2>
+              <h3 className="text-xs py-1 text-black">{note.created_at}</h3>
+              <div className="py-1 content text-base text-gray-800 overflow-hidden" dangerouslySetInnerHTML={{__html: note.notes}}></div>
+              <div>
+              </div>
             </div>
           </div>
+          </Link>
         </div>
       )) 
       : 
